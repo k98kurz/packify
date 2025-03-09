7979b42827017152782c452f0de7203ba9d250e9
Jonathan Voss <k98kurz@gmail.com>
2025-03-09 03:46:27 -0400

    significant refactor; ~50% space savings in my testing; completes and closes #2

--- packify/serialization.py
+++ packify/serialization.py
@@ -2,11 +2,71 @@ from __future__ import annotations
 from .errors import tressa
 from .interface import Packable
 from decimal import Decimal
+from enum import IntEnum
 from types import NoneType
+from math import ceil, log
 import struct
 
 
 SerializableType = Packable|dict|list|set|tuple|int|bool|float|Decimal|str|bytes|bytearray|NoneType
+
+
+class LengthCategory(IntEnum):
+    CAT0 = 0 << 6       # 00xxxxxx
+    CAT1 = 1 << 6       # 01xxxxxx
+    CAT2 = 2 << 6       # 10xxxxxx
+    CAT3 = 3 << 6       # 11xxxxxx
+
+    def fmt(self) -> str:
+        return {
+            LengthCategory.CAT0: 'x',
+            LengthCategory.CAT1: 'B',
+            LengthCategory.CAT2: 'H',
+            LengthCategory.CAT3: 'I'
+        }[self]
+
+    def fmt_count(self) -> int:
+        return {
+            LengthCategory.CAT0: 0,
+            LengthCategory.CAT1: 1,
+            LengthCategory.CAT2: 2,
+            LengthCategory.CAT3: 4
+        }[self]
+
+    @staticmethod
+    def for_len(n: int) -> LengthCategory:
+        if n < 2**8:
+            return LengthCategory.CAT1
+        elif n < 2**16:
+            return LengthCategory.CAT2
+        elif n < 2**32:
+            return LengthCategory.CAT3
+        else:
+            return LengthCategory.CAT0
+
+
+class EncodedType(IntEnum):
+    NONE      = 0
+    BYTES     = 1
+    BYTEARRAY = 2
+    STR       = 3
+    INT       = 4
+    BOOL      = 5
+    FLOAT     = 6
+    DECIMAL   = 7
+    LIST      = 10
+    SET       = 11
+    TUPLE     = 12
+    DICT      = 13
+    PACKABLE  = 20
+
+
+LENGTH_MASK = 0b11000000  # The two high bits
+TYPE_MASK   = 0b00111111  # The six low bits
+
+def decode(code: int) -> tuple[LengthCategory, EncodedType]:
+    """Decodes a code into a length category and encoded type."""
+    return LengthCategory(code & LENGTH_MASK), EncodedType(code & TYPE_MASK)
 
 
 def pack(data: SerializableType) -> bytes:
@@ -22,162 +82,240 @@ def pack(data: SerializableType) -> bytes:
         f'{type(data)} is not serializable')
 
     if isinstance(data, Packable):
-        packed = bytes(data.__class__.__name__, 'utf-8').hex()
-        packed = bytes(packed, 'utf-8') + b'_' + data.pack()
+        name = bytes(data.__class__.__name__, 'utf-8')
+        packed = data.pack()
+        category = LengthCategory.for_len(max(len(name), len(packed)))
+        code = category.value | EncodedType.PACKABLE.value
+
+        fmt = f'!B{category.fmt()}{category.fmt()}{len(name)}s{len(packed)}s'
+
         return struct.pack(
-            f'!1sI{len(packed)}s',
-            b'p',
+            fmt,
+            code,
+            len(name),
             len(packed),
+            name,
             packed
         )
 
     if type(data) in (list, set, tuple):
-        items = b''.join([pack(item) for item in data])
-        code = ({
-            list: b'l',
-            set: b'e',
-            tuple: b't'
+        items = [pack(item) for item in data]
+        count = len(items)
+        item_lens = [len(item) for item in items]
+        category = LengthCategory.for_len(count)
+        item_category = LengthCategory.for_len(max(item_lens))
+        code = category.value | ({
+            list: EncodedType.LIST,
+            set: EncodedType.SET,
+            tuple: EncodedType.TUPLE
         })[type(data)]
 
+        fmt = f'!BB{category.fmt()}{count}{item_category.fmt()}'
+        for il in item_lens:
+            fmt += f'{il}s'
+
         return struct.pack(
-            f'!1sI{len(items)}s',
+            fmt,
             code,
-            len(items),
-            items
+            item_category.value,
+            count,
+            *item_lens,
+            *items
         )
 
     if type(data) in (bytes, bytearray):
+        length = len(data)
+        category = LengthCategory.for_len(length)
+        code = category.value | ({
+            bytes: EncodedType.BYTES,
+            bytearray: EncodedType.BYTEARRAY
+        })[type(data)]
+
         return struct.pack(
-            f'!1sI{len(data)}s',
-            b'b' if type(data) is bytes else b'a',
-            len(data),
+            f'!B{category.fmt()}{length}s',
+            code,
+            length,
             data
         )
 
     if type(data) is str:
         data = bytes(data, 'utf-8')
+        length = len(data)
+        category = LengthCategory.for_len(length)
+        code = category.value | EncodedType.STR
+        fmt = f'!B{category.fmt()}{length}s'
+
         return struct.pack(
-            f'!1sI{len(data)}s',
-            b's',
-            len(data),
+            fmt,
+            code,
+            length,
             data
         )
 
     if type(data) is int:
+        category = LengthCategory.for_len(data)
+        code = category.value | EncodedType.INT
+
+        if category == LengthCategory.CAT0:
+            size = ceil(log(data, 2)/8)
+            data = data.to_bytes(size, 'big')
+            return struct.pack(
+                f'!BB{size}s',
+                code,
+                size,
+                data
+            )
+
         return struct.pack(
-            f'!1sII',
-            b'i',
-            4,
+            f'!B{category.fmt()}',
+            code,
             data
         )
 
     if type(data) is bool:
         return struct.pack(
-            f'!1sI?',
-            b'B',
-            1,
+            f'!B?',
+            EncodedType.BOOL.value,
             data
         )
 
     if type(data) is float:
         return struct.pack(
-            f'!1sId',
-            b'f',
-            8,
+            f'!Bd',
+            EncodedType.FLOAT.value,
             data
         )
 
     if type(data) is Decimal:
         data = bytes(str(data), 'utf-8')
+        length = len(data)
+        category = LengthCategory.for_len(length)
+        code = category.value | EncodedType.DECIMAL
+
         return struct.pack(
-            f'!1sI{len(data)}s',
-            b'D',
-            len(data),
+            f'!B{category.fmt()}{length}s',
+            code,
+            length,
             data
         )
 
     if type(data) is dict:
-        items = b''.join(sorted([
+        items = sorted([
             pack((key, value))
             for key, value in data.items()
-        ]))
+        ])
+        count = len(items)
+        item_lens = [len(item) for item in items]
+        category = LengthCategory.for_len(count)
+        item_category = LengthCategory.for_len(max(item_lens))
+        code = category.value | EncodedType.DICT
+
+        fmt = f'!BB{category.fmt()}{count}{item_category.fmt()}'
+        for il in item_lens:
+            fmt += f'{il}s'
+
         return struct.pack(
-            f'!1sI{len(items)}s',
-            b'd',
-            len(items),
-            items
+            fmt,
+            code,
+            item_category.value,
+            count,
+            *item_lens,
+            *items
         )
 
     if data is None:
-        return struct.pack(
-            f'!1sI',
-            b'n',
-            0
-        )
+        return struct.pack(f'!B', EncodedType.NONE.value)
 
 
 def unpack(data: bytes, inject: dict = {}) -> SerializableType:
     """Deserializes an instance of a Packable implementation
         or built-in type, recursively calling itself as necessary.
     """
-    code, data = struct.unpack(f'!1s{len(data)-1}s', data)
+    code, data = struct.unpack(f'!B{len(data)-1}s', data)
     dependencies = {**globals(), **inject}
 
-    if code == b'p':
-        packed_len, data = struct.unpack(f'!I{len(data)-4}s', data)
-        packed, _ = struct.unpack(f'!{packed_len}s{len(data)-packed_len}s', data)
-        packed_class, _, packed_data = packed.partition(b'_')
-        packed_class = str(bytes.fromhex(str(packed_class, 'utf-8')), 'utf-8')
+    category, encoded_type = decode(code)
+
+    if encoded_type == EncodedType.PACKABLE:
+        name_len, packed_len, data = struct.unpack(
+            f'!{category.fmt()}{category.fmt()}{len(data)-category.fmt_count()*2}s',
+            data
+        )
+        packed_class, packed_data, _ = struct.unpack(
+            f'!{name_len}s{packed_len}s{len(data)-name_len-packed_len}s',
+            data
+        )
+        packed_class = str(packed_class, 'utf-8')
         tressa(packed_class in dependencies,
             f'{packed_class} not found in globals or inject; cannot unpack')
         tressa(hasattr(dependencies[packed_class], 'unpack'),
             f'{packed_class} must have unpack method')
         return dependencies[packed_class].unpack(packed_data, inject=inject)
 
-    if code in (b'l', b'e', b't', b'd'):
-        let_len, data = struct.unpack(f'!I{len(data)-4}s', data)
-        let_data, _ = struct.unpack(f'!{let_len}s{len(data)-let_len}s', data)
-        items = []
-        while len(let_data) > 0:
-            _, item_len, _ = struct.unpack(f'!1sI{len(let_data)-5}s', let_data)
-            item, let_data = struct.unpack(
-                f'!{5+item_len}s{len(let_data)-5-item_len}s',
-                let_data
-            )
-            items.append(unpack(item, inject=inject))
+    if encoded_type in (EncodedType.LIST, EncodedType.SET, EncodedType.TUPLE, EncodedType.DICT):
+        item_category, count, data = struct.unpack(
+            f'!B{category.fmt()}{len(data)-category.fmt_count()-1}s',
+            data
+        )
+        item_category = LengthCategory(item_category)
+        item_lens = struct.unpack(
+            f'!{count}{item_category.fmt()}{len(data)-item_category.fmt_count()*count}s',
+            data
+        )
+        data = item_lens[-1]
+        item_lens = item_lens[:-1]
+        items = struct.unpack(
+            '!' + ''.join([f'{il}s' for il in item_lens]) + f'{len(data)-sum(item_lens)}s',
+            data
+        )
+        items = items[:-1]
+        items = [unpack(item, inject=inject) for item in items]
 
-        if code == b'l':
+        if encoded_type == EncodedType.LIST:
             return items
-        if code == b'e':
+        if encoded_type == EncodedType.SET:
             return set(items)
-        if code == b't':
+        if encoded_type == EncodedType.TUPLE:
             return tuple(items)
-        if code == b'd':
+        if encoded_type == EncodedType.DICT:
             return {pair[0]: pair[1] for pair in items}
 
-    if code in (b'b', b'a'):
-        bt_len, data = struct.unpack(f'!I{len(data)-4}s', data)
+    if encoded_type in (EncodedType.BYTES, EncodedType.BYTEARRAY):
+        bt_len, data = struct.unpack(
+            f'!{category.fmt()}{len(data)-category.fmt_count()}s', data
+        )
         bt_data, _ = struct.unpack(f'!{bt_len}s{len(data)-bt_len}s', data)
-        return bt_data if code == b'b' else bytearray(bt_data)
+        return bt_data if encoded_type == EncodedType.BYTES else bytearray(bt_data)
 
-    if code == b's':
-        s_len, data = struct.unpack(f'!I{len(data)-4}s', data)
+    if encoded_type == EncodedType.STR:
+        s_len, data = struct.unpack(
+            f'!{category.fmt()}{len(data)-category.fmt_count()}s', data
+        )
         s, _ = struct.unpack(f'!{s_len}s{len(data)-s_len}s', data)
         return str(s, 'utf-8')
 
-    if code == b'i':
-        return struct.unpack(f'!II{len(data)-8}s', data)[1]
+    if encoded_type == EncodedType.INT:
+        if category == LengthCategory.CAT0:
+            size, data = struct.unpack(f'!B{len(data)-1}s', data)
+            data, _ = struct.unpack(f'!{size}s{len(data)-size}s', data)
+            return int.from_bytes(data, 'big')
+        else:
+            return struct.unpack(
+                f'!{category.fmt()}{len(data)-category.fmt_count()}s', data
+            )[0]
 
-    if code == b'B':
-        return struct.unpack(f'!I?', data)[1]
+    if encoded_type == EncodedType.BOOL:
+        return struct.unpack(f'!?', data)[0]
 
-    if code == b'f':
-        return struct.unpack(f'!Id{len(data)-12}s', data)[1]
+    if encoded_type == EncodedType.FLOAT:
+        return struct.unpack(f'!d{len(data)-8}s', data)[0]
 
-    if code == b'D':
-        s_len, data = struct.unpack(f'!I{len(data)-4}s', data)
+    if encoded_type == EncodedType.DECIMAL:
+        s_len, data = struct.unpack(
+            f'!{category.fmt()}{len(data)-category.fmt_count()}s', data
+        )
         s, _ = struct.unpack(f'!{s_len}s{len(data)-s_len}s', data)
         return Decimal(str(s, 'utf-8'))
 
-    if code == b'n':
+    if encoded_type == EncodedType.NONE:
         return None
